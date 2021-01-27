717311d0fddec802141792e4785eb06a271adb4b
Artem Stupak <artem.st@sbtech.com>
2021-01-27 21:59:15 +0200

    uncomment wrong lines

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 
-// provider "aws" {
-//     region = "eu-west-1"
-// }
+ provider "aws" {
+     region = "eu-west-1"
+ }
 
-// data "aws_ami" "ubuntu" {
-//   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-//   filter {
-//     name   = "name"
-//     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-//   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-//   filter {
-//     name   = "virtualization-type"
-//     values = ["hvm"]
-//   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-//   owners = ["099720109477"] # Canonical
-// }
+  owners = ["099720109477"] # Canonical
+}
 
 // resource "aws_instance" "web" {
 //   ami           = data.aws_ami.ubuntu.id
